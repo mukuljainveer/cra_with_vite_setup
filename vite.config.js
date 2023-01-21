4c3402d7789dac683f2d88e1ce1fb209d6393c1d
Mukul Jain <mukuljainveer11@gmail.com>
2023-01-21 12:12:51 +0530

    enviroment file configuratin with vite and absolute path configurations

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react";
+import envCompatible from "vite-plugin-env-compatible";
+import path from 'path';
 
+console.log(path.resolve(__dirname, 'src'))
 export default defineConfig({
-    plugins:[react()]
-})
+  plugins: [react(), envCompatible()],
+  envPrefix: "REACT_APP",
+  esbuild: { loader: "jsx", include: /src\/.*\.jsx?$/, exclude: [] },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@screen', replacement: path.resolve(__dirname, 'src/screen') },
+    ],
+  },
+});
